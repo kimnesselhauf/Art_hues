eba5c6904c0151a1b57d7a3cb86bdd309963c312
Kim Nesselhauf <kim.nesselhauf@gmail.com>
2020-09-06 19:25:35 +0200

    webpack

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module ArtHues
     config.load_defaults 5.2
 
 config.assets.initialize_on_precompile = false
-
+config.assets.js_compressor = :uglifier
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
