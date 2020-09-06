54e1babb06f237c17405767361e7115f8f6c3e04
Kim Nesselhauf <kim.nesselhauf@gmail.com>
2020-09-06 18:29:31 +0200

    =falsse

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 # gitrequire_relative 'boot'
-
+config.assets.initialize_on_precompile = false
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -16,7 +16,7 @@ module ArtHues
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
